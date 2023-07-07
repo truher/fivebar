f1bab641d6851618f79dab91bfab54ad32814afa
Joel Truher <joel@truher.org>
2023-07-06 19:46:31 -0700

    whoops, also these files

--- python/kinematics.py
+++ python/kinematics.py
@@ -1,60 +1,73 @@
+"""Kinematics of 2-dof 5-bar planar linkage with one grounded bar.
+
+    Adapted from
+    http://charm.stanford.edu/ME327/JaredAndSam
+
+    Which is itself adapted from
+    "The Pantograph Mk-II: A Haptic Instrument" Hayward, 2005
+"""
 import numpy as np
+from scenario import Scenario
 
 
-def inverse(a1, a2, a3, a4, a5, x3, y3):
-    """Inverse kinematics of 2-DOF 5-bar linkage
+def inverse(scenario: Scenario, x3, y3):
+    """Inverse kinematics
 
-    a1-a5: link lengths, meters
+    scenario: simulation geometry
     x3,y3: position of end effector, meters
     """
 
     P13 = np.sqrt((x3**2) + (y3**2))
-    P53 = np.sqrt(((x3 + a5) ** 2) + (y3**2))
+    P53 = np.sqrt(((x3 + scenario.a5) ** 2) + (y3**2))
 
-    alphaOne = np.arccos(((a1**2) + (P13**2) - (a2**2)) / (2 * a1 * P13))
+    alphaOne = np.arccos(
+        ((scenario.a1**2) + (P13**2) - (scenario.a2**2)) / (2 * scenario.a1 * P13)
+    )
     betaOne = np.arctan2(y3, -x3)
     thetaOne = np.pi - alphaOne - betaOne
 
-    alphaFive = np.arctan2(y3, x3 + a5)
-    betaFive = np.arccos(((P53**2) + (a4**2) - (a3**2)) / (2 * P53 * a4))
+    alphaFive = np.arctan2(y3, x3 + scenario.a5)
+    betaFive = np.arccos(
+        ((P53**2) + (scenario.a4**2) - (scenario.a3**2)) / (2 * P53 * scenario.a4)
+    )
     thetaFive = alphaFive + betaFive
 
     return thetaOne, thetaFive
 
 
-def forward(a1, a2, a3, a4, a5, t1, t5, ax, plot_linkage):
-    """Forward kinematics of 2-DOF 5-bar planar linkage
+def forward(scenario: Scenario, t1, t5, ax, plot_linkage):
+    """Forward kinematics
 
-    Following the work of: "The Pantograph Mk-II: A Haptic Instrument"
-    Hayward, 2005 (note two negative signs in the Jacobian are fixed here)
-
-    a1-a5: link lengths, meters
-    t1: ?
+    scenario: geometry
+    t1: angle between a1 and a5
+    t5: angle between a4 and a5
     ax: matplotlib axes to plot on
     plot_linkage (boolean): do the plotting
+
+    returns the jacobian.  todo make that a separate function
     """
 
     # by definition
-    x1 = 0
-    y1 = 0
+    x1 = scenario.x1
+    y1 = scenario.y1
 
-    x2 = a1 * np.cos(t1)
-    y2 = a1 * np.sin(t1)
+    x2 = scenario.a1 * np.cos(t1)
+    y2 = scenario.a1 * np.sin(t1)
 
-    x4 = a4 * np.cos(t5) - a5
-    y4 = a4 * np.sin(t5)
+    x4 = scenario.a4 * np.cos(t5) - scenario.a5
+    y4 = scenario.a4 * np.sin(t5)
 
-    x5 = -a5
+    x5 = -scenario.a5
     y5 = 0
 
     P2 = np.array([[x2], [y2]])
     P4 = np.array([[x4], [y4]])
 
-    P2Ph = (a2**2 - a3**2 + np.linalg.norm(P4 - P2) ** 2) / (
+    P2Ph = (scenario.a2**2 - scenario.a3**2 + np.linalg.norm(P4 - P2) ** 2) / (
         2 * np.linalg.norm(P4 - P2)
     )
     Ph = P2 + (P2Ph / np.linalg.norm(P2 - P4)) * (P4 - P2)
-    P3Ph = np.sqrt(a2**2 - P2Ph**2)
+    P3Ph = np.sqrt(scenario.a2**2 - P2Ph**2)
 
     x3 = Ph[0].item() + (P3Ph / np.linalg.norm(P2 - P4)) * (y4 - y2)
     y3 = Ph[1].item() - (P3Ph / np.linalg.norm(P2 - P4)) * (x4 - x2)
@@ -78,10 +91,12 @@ def forward(a1, a2, a3, a4, a5, t1, t5, ax, plot_linkage):
     b = np.linalg.norm(P2 - Ph)
     h = np.linalg.norm(P3 - Ph)
 
-    del1_x2 = -a1 * np.sin(t1)  # NOTE: THE AUTHOR FORGOT NEGATIVE SIGN IN THE PAPER
-    del1_y2 = a1 * np.cos(t1)
-    del5_x4 = -a4 * np.sin(t5)  # NOTE: THE AUTHOR FORGOT NEGATIVE SIGN IN THE PAPER
-    del5_y4 = a4 * np.cos(t5)
+    # NOTE: THE AUTHOR FORGOT NEGATIVE SIGN IN THE PAPER
+    del1_x2 = -scenario.a1 * np.sin(t1)
+    del1_y2 = scenario.a1 * np.cos(t1)
+    # NOTE: THE AUTHOR FORGOT NEGATIVE SIGN IN THE PAPER
+    del5_x4 = -scenario.a4 * np.sin(t5)
+    del5_y4 = scenario.a4 * np.cos(t5)
 
     del1_y4 = 0
     del1_x4 = 0
@@ -90,7 +105,9 @@ def forward(a1, a2, a3, a4, a5, t1, t5, ax, plot_linkage):
 
     # joint 1
     del1_d = ((x4 - x2) * (del1_x4 - del1_x2) + (y4 - y2) * (del1_y4 - del1_y2)) / d
-    del1_b = del1_d - (del1_d * (a2**2 - a3**2 + d**2)) / (2 * d**2)
+    del1_b = del1_d - (del1_d * (scenario.a2**2 - scenario.a3**2 + d**2)) / (
+        2 * d**2
+    )
     del1_h = -b * del1_b / h
 
     del1_yh = (
@@ -117,7 +134,9 @@ def forward(a1, a2, a3, a4, a5, t1, t5, ax, plot_linkage):
 
     # joint 2
     del5_d = ((x4 - x2) * (del5_x4 - del5_x2) + (y4 - y2) * (del5_y4 - del5_y2)) / d
-    del5_b = del5_d - (del5_d * (a2**2 - a3**2 + d**2)) / (2 * d**2)
+    del5_b = del5_d - (del5_d * (scenario.a2**2 - scenario.a3**2 + d**2)) / (
+        2 * d**2
+    )
     del5_h = -b * del5_b / h
 
     del5_yh = (
--- python/simulation.py
+++ python/simulation.py
@@ -2,116 +2,192 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.patches import Rectangle
 import kinematics
+from scenario import Scenario
 
 
-def side():
+def side(scenario: Scenario):
     """Simulates the device moving around the edges of the workspace.
-    Plots the device at the 4 corners."""
-
-    plot_linkage = True
-
-    # max current per motor
-    Imax = 6  # A
-    kt = 0.0234  # Nm/A
-    Tmax = Imax * kt * np.ones([2, 1])  # Nm
-
-    # link lengths
-    a1 = 0.25
-    a2 = 0.25
-    a3 = a2
-    a4 = a1
-    a5 = 0.1
-
-    # origin
-    x1 = 0
-    y1 = 0
-
-    # the rectangle
-    xcenter = -(x1 + a5) / 2
-    ycenter = 0.34
-    w = 0.2794
-    h = 0.2159
+    Plots the device at the 4 corners.
+    """
 
     # find solve inverse kinematics at the boundary of the paper
     number = 300
-    xpoints1 = np.linspace(xcenter, xcenter + w / 2, number)
-    xpoints2 = np.linspace(xcenter + w / 2, xcenter + w / 2, number)
-    xpoints3 = np.linspace(xcenter + w / 2, xcenter - w / 2, number)
-    xpoints4 = np.linspace(xcenter - w / 2, xcenter - w / 2, number)
-    xpoints5 = np.linspace(xcenter - w / 2, xcenter + w / 2, number)
+    xpoints1 = np.linspace(scenario.xcenter, scenario.xcenter + scenario.w / 2, number)
+    xpoints2 = np.linspace(
+        scenario.xcenter + scenario.w / 2, scenario.xcenter + scenario.w / 2, number
+    )
+    xpoints3 = np.linspace(
+        scenario.xcenter + scenario.w / 2, scenario.xcenter - scenario.w / 2, number
+    )
+    xpoints4 = np.linspace(
+        scenario.xcenter - scenario.w / 2, scenario.xcenter - scenario.w / 2, number
+    )
+    xpoints5 = np.linspace(
+        scenario.xcenter - scenario.w / 2, scenario.xcenter + scenario.w / 2, number
+    )
 
     xpoints = np.concatenate((xpoints1, xpoints2, xpoints3, xpoints4, xpoints5))
 
-    ypoints1 = np.linspace(ycenter, ycenter + h / 2, number)
-    ypoints2 = np.linspace(ycenter + h / 2, ycenter - h / 2, number)
-    ypoints3 = np.linspace(ycenter - h / 2, ycenter - h / 2, number)
-    ypoints4 = np.linspace(ycenter - h / 2, ycenter + h / 2, number)
-    ypoints5 = np.linspace(ycenter + h / 2, ycenter + h / 2, number)
+    ypoints1 = np.linspace(scenario.ycenter, scenario.ycenter + scenario.h / 2, number)
+    ypoints2 = np.linspace(
+        scenario.ycenter + scenario.h / 2, scenario.ycenter - scenario.h / 2, number
+    )
+    ypoints3 = np.linspace(
+        scenario.ycenter - scenario.h / 2, scenario.ycenter - scenario.h / 2, number
+    )
+    ypoints4 = np.linspace(
+        scenario.ycenter - scenario.h / 2, scenario.ycenter + scenario.h / 2, number
+    )
+    ypoints5 = np.linspace(
+        scenario.ycenter + scenario.h / 2, scenario.ycenter + scenario.h / 2, number
+    )
 
     ypoints = np.concatenate((ypoints1, ypoints2, ypoints3, ypoints4, ypoints5))
 
     theta = np.zeros([2, len(xpoints)])
     for i in range(len(xpoints)):
-        # #find joint angles
+        # find joint angles
         x3 = xpoints[i].item()
         y3 = ypoints[i].item()
 
-        theta = kinematics.inverse(a1, a2, a3, a4, a5, x3, y3)
+        theta = kinematics.inverse(scenario, x3, y3)
         t1 = theta[0].item()
         t5 = theta[1].item()
 
-    fig, axs = plt.subplots(2, 2)
+    # plots
 
-    theta = kinematics.inverse(a1, a2, a3, a4, a5, max(xpoints), max(ypoints))
+    plot_linkage = True
+
+    fig, axs = plt.subplots(2, 3)
+
+    theta = kinematics.inverse(scenario, max(xpoints), max(ypoints))
     t1 = theta[0]
     t5 = theta[1]
-    kinematics.forward(a1, a2, a3, a4, a5, t1, t5, axs[0, 0], plot_linkage)
+    kinematics.forward(scenario, t1, t5, axs[0, 0], plot_linkage)
     axs[0, 0].axis("equal")
-    axs[0, 0].set_xlim(-0.25, 0.35)
-    axs[0, 0].set_ylim(-0.45, 0.03)
+    axs[0, 0].set_xlim(scenario.xmin, scenario.xmax)
+    axs[0, 0].set_ylim(scenario.ymin, scenario.ymax)
     axs[0, 0].grid()
     axs[0, 0].add_patch(
-        Rectangle((-(xcenter + w / 2), -(ycenter + h / 2)), w, h, fill=False)
+        Rectangle(
+            (
+                -(scenario.xcenter + scenario.w / 2),
+                -(scenario.ycenter + scenario.h / 2),
+            ),
+            scenario.w,
+            scenario.h,
+            fill=False,
+        )
     )
+    # axs[0,0].autoscale_view()
 
-    theta = kinematics.inverse(a1, a2, a3, a4, a5, min(xpoints), max(ypoints))
+    theta = kinematics.inverse(scenario, scenario.xcenter, np.min(ypoints))
     t1 = theta[0]
     t5 = theta[1]
-    kinematics.forward(a1, a2, a3, a4, a5, t1, t5, axs[0, 1], plot_linkage)
+    kinematics.forward(scenario, t1, t5, axs[0, 1], plot_linkage)
     axs[0, 1].axis("equal")
-    axs[0, 1].set_xlim(-0.25, 0.35)
-    axs[0, 1].set_ylim(-0.45, 0.03)
+    axs[0, 1].set_xlim(scenario.xmin, scenario.xmax)
+    axs[0, 1].set_ylim(scenario.ymin, scenario.ymax)
     axs[0, 1].grid()
     axs[0, 1].add_patch(
-        Rectangle((-(xcenter + w / 2), -(ycenter + h / 2)), w, h, fill=False)
+        Rectangle(
+            (
+                -(scenario.xcenter + scenario.w / 2),
+                -(scenario.ycenter + scenario.h / 2),
+            ),
+            scenario.w,
+            scenario.h,
+            fill=False,
+        )
     )
+    # axs[0,1].autoscale_view()
 
-    theta = kinematics.inverse(a1, a2, a3, a4, a5, max(xpoints), min(ypoints))
+    theta = kinematics.inverse(scenario, min(xpoints), max(ypoints))
     t1 = theta[0]
     t5 = theta[1]
-    kinematics.forward(a1, a2, a3, a4, a5, t1, t5, axs[1, 0], plot_linkage)
+    kinematics.forward(scenario, t1, t5, axs[0, 2], plot_linkage)
+    axs[0, 2].axis("equal")
+    axs[0, 2].set_xlim(scenario.xmin, scenario.xmax)
+    axs[0, 2].set_ylim(scenario.ymin, scenario.ymax)
+    axs[0, 2].grid()
+    axs[0, 2].add_patch(
+        Rectangle(
+            (
+                -(scenario.xcenter + scenario.w / 2),
+                -(scenario.ycenter + scenario.h / 2),
+            ),
+            scenario.w,
+            scenario.h,
+            fill=False,
+        )
+    )
+    axs[0, 2].autoscale_view()
+
+    theta = kinematics.inverse(scenario, max(xpoints), min(ypoints))
+    t1 = theta[0]
+    t5 = theta[1]
+    kinematics.forward(scenario, t1, t5, axs[1, 0], plot_linkage)
     axs[1, 0].axis("equal")
-    axs[1, 0].set_xlim(-0.25, 0.35)
-    axs[1, 0].set_ylim(-0.45, 0.03)
+    axs[1, 0].set_xlim(scenario.xmin, scenario.xmax)
+    axs[1, 0].set_ylim(scenario.ymin, scenario.ymax)
     axs[1, 0].grid()
     axs[1, 0].add_patch(
-        Rectangle((-(xcenter + w / 2), -(ycenter + h / 2)), w, h, fill=False)
+        Rectangle(
+            (
+                -(scenario.xcenter + scenario.w / 2),
+                -(scenario.ycenter + scenario.h / 2),
+            ),
+            scenario.w,
+            scenario.h,
+            fill=False,
+        )
     )
+    # axs[1,0].autoscale_view()
 
-    theta = kinematics.inverse(a1, a2, a3, a4, a5, min(xpoints), min(ypoints))
+    theta = kinematics.inverse(scenario, scenario.xcenter, np.max(ypoints))
     t1 = theta[0]
     t5 = theta[1]
-    kinematics.forward(a1, a2, a3, a4, a5, t1, t5, axs[1, 1], plot_linkage)
+    kinematics.forward(scenario, t1, t5, axs[1, 1], plot_linkage)
     axs[1, 1].axis("equal")
-    axs[1, 1].set_xlim(-0.25, 0.35)
-    axs[1, 1].set_ylim(-0.45, 0.03)
+    axs[1, 1].set_xlim(scenario.xmin, scenario.xmax)
+    axs[1, 1].set_ylim(scenario.ymin, scenario.ymax)
     axs[1, 1].grid()
     axs[1, 1].add_patch(
-        Rectangle((-(xcenter + w / 2), -(ycenter + h / 2)), w, h, fill=False)
+        Rectangle(
+            (
+                -(scenario.xcenter + scenario.w / 2),
+                -(scenario.ycenter + scenario.h / 2),
+            ),
+            scenario.w,
+            scenario.h,
+            fill=False,
+        )
     )
+    # axs[1,1].autoscale_view()
+
+    theta = kinematics.inverse(scenario, min(xpoints), min(ypoints))
+    t1 = theta[0]
+    t5 = theta[1]
+    kinematics.forward(scenario, t1, t5, axs[1, 2], plot_linkage)
+    axs[1, 2].axis("equal")
+    axs[1, 2].set_xlim(scenario.xmin, scenario.xmax)
+    axs[1, 2].set_ylim(scenario.ymin, scenario.ymax)
+    axs[1, 2].grid()
+    axs[1, 2].add_patch(
+        Rectangle(
+            (
+                -(scenario.xcenter + scenario.w / 2),
+                -(scenario.ycenter + scenario.h / 2),
+            ),
+            scenario.w,
+            scenario.h,
+            fill=False,
+        )
+    )
+    # axs[1,2].autoscale_view()
 
 
-def interior():
+def interior(scenario):
     """Plots the jacobian and minimum-maximum forces over the workspace."""
 
     # plot the linkage at each step
@@ -121,38 +197,14 @@ def interior():
     angle_number = 20
     angle_range = 2 * np.pi / angle_number
 
-    # max current per motor
-    kt = 0.0234
-
-    # based on stall torue (max peak)
-    Tmax = 0.129
-    Imax = Tmax / kt
-
-    # capstan mechanial advantage ratio
-    # ratio = 0.07303 / (0.0131 / 2)
-    ratio = 1
-
-    # link lengths
-    a1 = 0.25
-    a2 = 0.25
-    a3 = a2
-    a4 = a1
-    a5 = 0.1
-
-    # origin
-    x1 = 0
-    y1 = 0
-
-    # the rectangle
-    xcenter = -(x1 + a5) / 2
-    ycenter = 0.34
-    w = 0.2794
-    h = 0.2159
-
     # define how many points to solve for
     number = 10
-    xpoints = np.linspace(xcenter + w / 2, xcenter - w / 2, number)
-    ypoints = np.linspace(ycenter + h / 2, ycenter - h / 2, number)
+    xpoints = np.linspace(
+        scenario.xcenter + scenario.w / 2, scenario.xcenter - scenario.w / 2, number
+    )
+    ypoints = np.linspace(
+        scenario.ycenter + scenario.h / 2, scenario.ycenter - scenario.h / 2, number
+    )
 
     # preallocate
     condition = np.zeros([len(xpoints), len(ypoints)])
@@ -167,12 +219,12 @@ def interior():
             x3 = xpoints[i].item()
             y3 = ypoints[j].item()
 
-            theta = kinematics.inverse(a1, a2, a3, a4, a5, x3, y3)
+            theta = kinematics.inverse(scenario, x3, y3)
             t1 = theta[0].item()
             t5 = theta[1].item()
 
             # jacobian
-            J = kinematics.forward(a1, a2, a3, a4, a5, t1, t5, None, plot_linkage)
+            J = kinematics.forward(scenario, t1, t5, None, plot_linkage)
 
             Jinv = np.linalg.inv(J)
 
@@ -198,7 +250,6 @@ def interior():
                         * np.array(
                             [
                                 [np.cos(k * angle_range), -np.sin(k * angle_range)],
-                                # [np.cos(angle_range), -np.sin(k * angle_range)],
                                 [np.sin(k * angle_range), np.cos(k * angle_range)],
                             ]
                         )
@@ -209,8 +260,8 @@ def interior():
                     torque = np.transpose(J) @ f
                     # if we have  exceeded spec (90% of max current)
                     if (
-                        abs(torque[0].item()) >= (Tmax) * ratio
-                        or abs(torque[1].item()) >= (Tmax) * ratio
+                        abs(torque[0].item()) >= (scenario.Tmax) * scenario.ratio
+                        or abs(torque[1].item()) >= (scenario.Tmax) * scenario.ratio
                     ):
                         # then we have reached the max force in that direction
                         max_force_reached = True
@@ -226,17 +277,29 @@ def interior():
     plt.figure()
     ax = plt.gca()
     ax.axis("equal")
-    ax.set_xlim(-0.2, 0.35)
-    ax.set_ylim(-0.45, 0.03)
+    ax.set_xlim(scenario.xmin, scenario.xmax)
+    ax.set_ylim(scenario.ymin, scenario.ymax)
     ax.grid()
 
-    t1iso = 0.8719
-    t5iso = 2.2697
-    J = kinematics.forward(a1, a2, a3, a4, a5, t1iso, t5iso, ax, True)
+    # t1iso = 0.8719
+    # t5iso = 2.2697
+    # J = kinematics.forward(scenario, t1iso, t5iso, ax, False)
 
-    ax.add_patch(Rectangle((-(xcenter + w / 2), -(ycenter + h / 2)), w, h, fill=False))
+    ax.add_patch(
+        Rectangle(
+            (
+                -(scenario.xcenter + scenario.w / 2),
+                -(scenario.ycenter + scenario.h / 2),
+            ),
+            scenario.w,
+            scenario.h,
+            fill=False,
+        )
+    )
+    # ax.autoscale_view()
 
-    CS = ax.contour(-xpoints, -ypoints, np.transpose(condition))
+    CS = ax.contourf(-xpoints, -ypoints, np.transpose(condition), cmap="summer")
+    CS = ax.contour(-xpoints, -ypoints, np.transpose(condition), colors="k")
     ax.clabel(CS)
 
     ax.set_title("condition")
@@ -244,26 +307,78 @@ def interior():
     plt.figure()
     ax = plt.gca()
     ax.axis("equal")
-    ax.set_xlim(-0.2, 0.35)
-    ax.set_ylim(-0.45, 0.03)
+    ax.set_xlim(scenario.xmin, scenario.xmax)
+    ax.set_ylim(scenario.ymin, scenario.ymax)
     ax.grid()
 
     # plot one linkage configuration
-    t1iso = 0.8719
-    t5iso = 2.2697
-    J = kinematics.forward(a1, a2, a3, a4, a5, t1iso, t5iso, ax, True)
+    # t1iso = 0.8719
+    # t5iso = 2.2697
+    # J = kinematics.forward(scenario, t1iso, t5iso, ax, False)
 
-    ax.add_patch(Rectangle((-(xcenter + w / 2), -(ycenter + h / 2)), w, h, fill=False))
+    ax.add_patch(
+        Rectangle(
+            (
+                -(scenario.xcenter + scenario.w / 2),
+                -(scenario.ycenter + scenario.h / 2),
+            ),
+            scenario.w,
+            scenario.h,
+            fill=False,
+        )
+    )
+    # ax.autoscale_view()
 
-    CS = ax.contour(-xpoints, -ypoints, np.transpose(min_force))
+    CS = ax.contourf(-xpoints, -ypoints, np.transpose(min_force), cmap="summer")
+    CS = ax.contour(-xpoints, -ypoints, np.transpose(min_force), colors="k")
     ax.clabel(CS)
 
-    ax.set_title("min force")
+    ax.set_title("min force (N)")
 
 
 def main():
-    side()
-    interior()
+    scenario = Scenario(
+        name="foo",
+        a1=0.065,
+        a2=0.1,
+        a3=0.1,
+        a4=0.065,
+        a5=0.05,
+        x1=0,
+        y1=0,
+        ratio=1,
+        Tmax=0.19,
+        w=0.15,
+        h=0.075,
+        xcenter=-0.025,
+        ycenter=0.08,
+        xmin=-0.1,
+        xmax=0.15,
+        ymin=-0.15,
+        ymax=0.05,
+    )
+    # TODO theres some wierd reversing going on here
+    # original_scenario = Scenario(
+    #    name="foo",
+    #    a1=0.25,
+    #    a2=0.25,
+    #    a3=0.25,
+    #    a4=0.25,
+    #    a5=0.1,
+    #    x1=0,
+    #    y1=0,
+    #    # capstan mechanial advantage ratio
+    #    # ratio = 0.07303 / (0.0131 / 2)
+    #    ratio=1,
+    #    # stall torue Nm
+    #    Tmax=0.129,
+    #    w=0.2794,
+    #    h=0.2159,
+    #    xcenter=-0.05,
+    #    ycenter=0.34,
+    # )
+    side(scenario)
+    interior(scenario)
     plt.show()
 
 
